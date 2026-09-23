93ebabd06adf973bfbb3a917115ebfd64ccbcf60
Punsara Bandara <punsarab@unleashideas.com>
2026-09-23 13:40:01 +0530

    synlio ai init

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "hello synlio ai"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
